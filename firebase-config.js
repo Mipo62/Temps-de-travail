3e97c5dfc0c4e2d4d0e30766800b3ddc02e2179b
MiP0 <128356837+Mipo62@users.noreply.github.com>
2026-01-08 23:23:25 +0100

    Add Firebase configuration and initialization

---
+++ firebase-config.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD4anyZUy4jmXG1f6nvuWzphHdtR1mYg2c",
+  authDomain: "working-time-33018.firebaseapp.com",
+  projectId: "working-time-33018",
+  storageBucket: "working-time-33018.firebasestorage.app",
+  messagingSenderId: "172774543780",
+  appId: "1:172774543780:web:3827b920e202c4c94bf131"
+};
+
+// Initialisation
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
